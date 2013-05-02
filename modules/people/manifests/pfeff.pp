d067198f8a44d6b2e3567b3a94bd6a80e3767d37
Matt Pfefferle <matthew.pfefferle@gmail.com>
2013-05-02 15:33:07 -0400

    Mysql

--- modules/people/manifests/pfeff.pp
+++ modules/people/manifests/pfeff.pp
@@ -1,5 +1,6 @@
 class people::pfeff {
     include erlang
+    include mysql
     include oh-my-zsh
     include skype
     include vim
@@ -8,4 +9,7 @@ class people::pfeff {
 
     vim::bundle { 'vim-ruby/vim-ruby': }
     vim::bundle { 'rodjek/vim-puppet': }
+
+    mysql::db { 'ident_test': }
+
 }
